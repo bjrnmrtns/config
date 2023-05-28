b322186c63accc3eeaac5581bd1b351aa7ad9955
Bjorn Martens <bjorn@expeditious.nl>
2023-05-28 19:51:11 +0200

    add partition script

---
+++ partition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DISK=vda
+
+sudo parted /dev/${DISK} -- mklabel msdos
+sudo parted /dev/${DISK} -- mkpart primary 1MiB -8GiB
+sudo parted /dev/${DISK} -- mkpart primary linux-swap -8GiB 100%
+
+sudo mkfs.ext4 -L nixos /dev/${DISK}1
+sudo mkswap -L swap /dev/${DISK}2
+
+sudo mount /dev/disk/by-label/nixos /mnt
+sudo swapon /dev/${DISK}2
+sudo nixos-generate-config --root /mnt
+
