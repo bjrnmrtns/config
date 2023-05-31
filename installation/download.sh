e59c4dd8917217f8d6e569a732847d3178bc8951
Bjorn Martens <bjorn@expeditious.nl>
2023-05-31 09:35:38 +0200

    adapt download script

--- installation/download.sh
+++ installation/download.sh
@@ -1,3 +1,3 @@
-curl https://raw.githubusercontent.com/bjrnmrtns/nixos-config/installation/partition.sh > partition.sh
-curl https://raw.githubusercontent.com/bjrnmrtns/nixos-config/installation/download.sh > download.sh
-curl https://raw.githubusercontent.com/bjrnmrtns/nixos-config/installation/configuration.nix > configuration.nix
+curl https://raw.githubusercontent.com/bjrnmrtns/master/nixos-config/installation/partition.sh > partition.sh
+curl https://raw.githubusercontent.com/bjrnmrtns/master/nixos-config/installation/download.sh > download.sh
+curl https://raw.githubusercontent.com/bjrnmrtns/master/nixos-config/installation/configuration.nix > configuration.nix
