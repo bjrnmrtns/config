6bfeed8df7fa92c3715b9ae5ae055c0f81edf6d9
Bjorn Martens <bjorn@expeditious.nl>
2023-06-04 16:53:47 +0200

    start using module for home configuration

--- flake.nix
+++ flake.nix
@@ -1,15 +1,23 @@
 {
-  description = "NixOS system flake";
+  description = "NixOS system config in flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = inputs@{ self, nixpkgs, home-manager, flake-utils, ... }: {
     nixosConfigurations = {
       "mcfly" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-
 	modules = [
 	  ./configuration.nix
+	  home-manager.nixosModules.home-manager {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.bjorn = import ./home.nix;
+	  }
 	];
       };
     };
---
+++ home.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }: {
+  home.username = "bjorn";
+  home.homeDirectory = "/home/bjorn";
+  home.packages = [
+    pkgs.git
+  ];
+  programs.git = {
+    enable = true;
+    userName =  "Bjorn Martens";
+    userEmail =  "bjorn@expeditious.nl";
+  };
+  home.stateVersion = "23.05";
+  programs.home-manager.enable = true;
+}
