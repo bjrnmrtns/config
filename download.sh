454108dd160e3276dfcc6b0abbae44ce4eaf54fa
Bjorn Martens <bjorn@expeditious.nl>
2023-06-05 19:34:34 +0200

    add partition uefi script

--- download.sh
+++ download.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 curl https://raw.githubusercontent.com/bjrnmrtns/nixos-config/master/partition.sh --output partition.sh
+curl https://raw.githubusercontent.com/bjrnmrtns/nixos-config/master/partition-uefi.sh --output partition-uefi.sh
 curl https://raw.githubusercontent.com/bjrnmrtns/nixos-config/master/configuration.nix --output configuration.nix
 curl https://raw.githubusercontent.com/bjrnmrtns/nixos-config/master/readme.md --output readme.md
---
+++ partition-uefi.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Run lsblk to change disk and make sure you use the right one as it is being formatted
+
+DISK=vda
+
+sudo parted /dev/${DISK} -- mklabel gpt
+sudo parted /dev/${DISK} -- mkpart primary 512MiB -8GiB
+sudo parted /dev/${DISK} -- mkpart primary linux-swap -8GiB 100%
+sudo parted /dev/${DISK} -- mkpart ESP fat32 1MiB 512MiB
+
+sudo mkfs.ext4 -L nixos /dev/${DISK}1
+sudo mkswap -L swap /dev/${DISK}2
+sudo mkfs.fat -F 32 -n boot /dev/${DISK}3
+
+sudo mount /dev/disk/by-label/nixos /mnt
+sudo mkdir -p /mnt/boot
+sudo mount /dev/disk/by-label/boot /mnt/boot
+sudo swapon /dev/${DISK}2
+sudo nixos-generate-config --root /mnt
+
+
