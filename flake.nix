db71a2f089f5dd5c878fc903818c6e812cf2f728
Bjorn Martens <bjorn@expeditious.nl>
2023-06-08 20:04:31 +0200

    add agenix for secret storing

--- flake.nix
+++ flake.nix
@@ -3,23 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    agenix.url = "github:ryantm/agenix";
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
+    secrets = {
+      url = "git+ssh://gitolite@mcfly:/bjorn/nixos-secrets?ref=master";
+      flake = false;
+    };
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, flake-utils, ... }: {
+  outputs = { self, nixpkgs, home-manager, agenix, flake-utils, ... }@inputs: {
     nixosConfigurations = {
       "mcfly" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 	modules = [
+	  {
+	    # TODO: This x86_64-linux is wrong should be a system variable
+	    environment.systemPackages = [ agenix.packages.x86_64-linux.default ];
+          }
 	  ./configuration.nix
 	  ./modules/gitolite.nix
+	  agenix.nixosModules.default
 	  home-manager.nixosModules.home-manager {
 	    home-manager.useGlobalPkgs = true;
 	    home-manager.useUserPackages = true;
 	    home-manager.users.bjorn = import ./home.nix;
 	  }
 	];
+	specialArgs = { inherit (inputs) secrets; };
       };
     };
   };
