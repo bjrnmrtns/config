635fbf5841067a010d9a0203a5980009d3d23352
Bjorn Martens <bjorn@expeditious.nl>
2023-06-09 14:56:11 +0200

    add more stuff to specific host configuration

--- flake.nix
+++ flake.nix
@@ -16,25 +16,19 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, agenix, flake-utils, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, flake-utils, ... }: {
     nixosConfigurations = {
 
       mcfly = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 	modules = [ ./hosts/mcfly/configuration.nix
-	  {
-	    # TODO: This x86_64-linux is wrong should be a system variable
-	    environment.systemPackages = [ agenix.packages.x86_64-linux.default ];
-          }
-	  ./modules/gitolite.nix
-	  agenix.nixosModules.default
 	  home-manager.nixosModules.home-manager {
 	    home-manager.useGlobalPkgs = true;
 	    home-manager.useUserPackages = true;
 	    home-manager.users.bjorn = import ./home.nix;
 	  }
 	];
-	specialArgs = { inherit (inputs) secrets; };
+	specialArgs = { inherit (inputs) agenix secrets; };
       };
 
     };
