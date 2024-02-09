7a07f8106ca93baa14b955df24067cbbf5cdfec5
Bjorn Martens <bjorn@expeditious.nl>
2024-02-09 15:53:56 +0100

    add tmux-sessionizer

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+# config links
 ln -s $(readlink -f nvim) ${HOME}/.config/nvim 
 ln -s $(readlink -f sway) ${HOME}/.config/sway
 ln -s $(readlink -f waybar) ${HOME}/.config/waybar
 ln -s $(readlink -f gitconfig) ${HOME}/.gitconfig
+ln -s $(readlink -f tmux.conf) ${HOME}/.tmux.conf
+
+# executable links
+ln -s $(readlink -f bin/tmux-sessionizer) ${HOME}/.local/bin/tmux-sessionizer
